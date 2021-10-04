a56d2a3e4e7f6637970e08df8682b8d673c853aa
Georgi <georgidim30@gmail.com>
2021-10-04 22:56:23 +0300

    Fix seeders

--- database/seeds/Users/UsersSeeder.php
+++ database/seeds/Users/UsersSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory;
 
 class UsersSeeder extends Seeder
 {
@@ -19,5 +20,27 @@ class UsersSeeder extends Seeder
             'password' => bcrypt('123321'),
             'cl_role_id' => $role->id,
         ]);
+
+        self::otherUsers();
+    }
+
+    /* Create other users */
+    private static function otherUsers() {
+        if (config('app.env') == 'local') {
+            $faker = Factory::create();
+
+            $users = [];
+            for ($i = 1; $i <= 10; $i++) {
+                $users[] = [
+                    'name' => $faker->firstNameFemale,
+                    'email' => $i > 6 ? 'lecturer' . ($i - 6) . '@test.test' : 'test' . $i . '@test.test',
+                    'last_name' => $faker->lastName,
+                    'password' => bcrypt('123456789'),
+                    'cl_role_id' => $i > 6 ? 3 : 2,
+                ];
+            }
+
+            App\User::insert($users);
+        }
     }
 }
