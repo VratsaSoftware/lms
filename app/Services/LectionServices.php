e329ba0f07cba66aa68f563c26fe3a417d840dfb
georgi2003 <georgidim30@gmail.com>
2021-10-26 10:56:45 +0300

    Fix parse video url

--- app/Services/LectionServices.php
+++ app/Services/LectionServices.php
@@ -9,15 +9,8 @@ class LectionServices {
         if ($video) {
             $urls = explode(' ', $video->url);
             foreach ($urls as $url) {
-                if (strstr($video->url, 'http')) {
-//                    if (strstr($video->url, 'watch?v=')) {
-//                        $videoUrl = str_replace("watch?v=", "embed/", $url);
-//                        $videoUrls[] = str_replace(['&feature=youtu.be', '&ab_channel=VratsaSoftwareSchool', '&list=PLQFk-VQC2oBZFnDSUuScF8QRm5PHJTeNP&index=2'], '', $videoUrl);
-//                    } else {
-//                        $videoUrls[] = str_replace("youtu.be", "www.youtube.com/embed", $url);
-//                    }
-
-                    $newUrl = explode('=', $video->url);
+                if (strstr($url, 'http')) {
+                    $newUrl = explode('=', $url);
                     $newUrl = explode('&', $newUrl[1]);
                     $videoUrls[] = 'https://www.youtube.com/embed/' . $newUrl[0];
                 }
