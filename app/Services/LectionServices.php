25671cde766fb19686b08fb8b3eee11d8f860661
georgi2003 <georgidim30@gmail.com>
2021-10-26 10:32:32 +0300

    Fix student show videos

--- app/Services/LectionServices.php
+++ app/Services/LectionServices.php
@@ -9,11 +9,13 @@ class LectionServices {
         if ($video) {
             $urls = explode(' ', $video->url);
             foreach ($urls as $url) {
-                if (strstr($video->url, 'watch?v=')) {
-                    $videoUrl = str_replace("watch?v=", "embed/", $url);
-                    $videoUrls[] = str_replace(['&feature=youtu.be', '&ab_channel=VratsaSoftwareSchool'], '', $videoUrl);
-                } else {
-                    $videoUrls[] = str_replace("youtu.be", "www.youtube.com/embed", $url);
+                if (strstr($video->url, 'http')) {
+                    if (strstr($video->url, 'watch?v=')) {
+                        $videoUrl = str_replace("watch?v=", "embed/", $url);
+                        $videoUrls[] = str_replace(['&feature=youtu.be', '&ab_channel=VratsaSoftwareSchool', '&list=PLQFk-VQC2oBZFnDSUuScF8QRm5PHJTeNP&index=2'], '', $videoUrl);
+                    } else {
+                        $videoUrls[] = str_replace("youtu.be", "www.youtube.com/embed", $url);
+                    }
                 }
             }
         }
