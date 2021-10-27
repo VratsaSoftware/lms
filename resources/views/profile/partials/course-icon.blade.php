c223d20b3d9534ce652ee21e22cfe2d6d441b366
georgi2003 <georgidim30@gmail.com>
2021-10-27 12:30:25 +0300

    Add qa svg icon

--- resources/views/profile/partials/course-icon.blade.php
+++ resources/views/profile/partials/course-icon.blade.php
@@ -1,11 +1,11 @@
 @if ($type == 1)
-	<img src="{{ asset('assets/icons/Programirane.svg') }}" alt="#">
+	<img src="{{ asset('assets/icons/Programirane.svg') }}" alt="">
 @elseif ($type == 2)
-	<img src="{{ asset('assets/icons/Design.svg') }}" alt="#">
+	<img src="{{ asset('assets/icons/Design.svg') }}" alt="">
 @elseif ($type == 3)
-	<img src="{{ asset('assets/icons/Marketing.svg') }}" alt="#">
+	<img src="{{ asset('assets/icons/Marketing.svg') }}" alt="">
 @elseif ($type == 4)
-	<img src="{{ asset('assets/icons/Software.svg') }}" alt="#">
+	<img src="{{ asset('assets/icons/Software.svg') }}" alt="">
 @elseif ($type == 5)
-    <img src="{{ asset('assets/icons/qa.svg') }}" alt="#" width="80px">
+    <img src="{{ asset('assets/icons/qa.svg') }}" alt="" width="80px">
 @endif
