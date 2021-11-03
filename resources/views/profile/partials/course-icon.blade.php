d8b00753b079c1b461dc2d36f0642ad6810015b2
georgi2003 <georgidim30@gmail.com>
2021-11-03 08:06:33 +0200

    Fix course icons

--- resources/views/profile/partials/course-icon.blade.php
+++ resources/views/profile/partials/course-icon.blade.php
@@ -1,11 +1,11 @@
 @if ($type == 1)
-    <img src="{{ asset('assets/icons/Programirane.svg') }}" height="40px" alt="">
+    <img src="{{ asset('assets/icons/Programirane.svg') }}" height="70px" alt="">
 @elseif ($type == 2)
-    <img src="{{ asset('assets/icons/Design.svg') }}" height="40px" alt="">
+    <img src="{{ asset('assets/icons/Design.svg') }}" height="70px" alt="">
 @elseif ($type == 3)
-    <img src="{{ asset('assets/icons/Marketing.svg') }}" height="40px" alt="">
+    <img src="{{ asset('assets/icons/Marketing.svg') }}" height="70px" alt="">
 @elseif ($type == 4)
-    <img src="{{ asset('assets/icons/Software.svg') }}" height="40px" alt="">
+    <img src="{{ asset('assets/icons/Software.svg') }}" height="70px" alt="">
 @elseif ($type == 5)
-    <img src="{{ asset('assets/icons/qa.svg') }}" alt="" height="40px" width="80px">
+    <img src="{{ asset('assets/icons/qa.svg') }}" alt="" height="70px" width="80px">
 @endif
