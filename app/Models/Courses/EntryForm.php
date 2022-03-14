20eae8199f23ed5a217187898a43c3fe043543c6
georgi2003 <georgidim30@gmail.com>
2022-03-14 15:27:51 +0200

    Entry add SoftDeletes

--- app/Models/Courses/EntryForm.php
+++ app/Models/Courses/EntryForm.php
@@ -3,9 +3,12 @@
 namespace App\Models\Courses;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EntryForm extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'entry_forms';
     protected $guarded = [];
 
