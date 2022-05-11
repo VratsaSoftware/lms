303d12f9aac94d17091b665681ffb9efdafb38ed
georgi2003 <georgidim30@gmail.com>
2022-05-11 09:44:20 +0300

    Course module homework statistics - fix title

--- resources/views/course/module/homeworks.blade.php
+++ resources/views/course/module/homeworks.blade.php
@@ -13,9 +13,12 @@
     <div class="col-xl me-3 pt-md-5 mt-md-4 tab-content edit-content-admin" style="width: 30%!important;" id="elements-container">
         <div class="row g-0">
             <div class="col">
-                <h1 class="admin-text-2 title-text text-uppercase mb-5">
-                    Домашни - {{ $module->Course->name }}, {{ $module->name }}
+                <h1 class="admin-text-2 title-text text-uppercase">
+                    Домашни - {{ $module->Course->name }}
                 </h1>
+            </div>
+            <div class="col-12 mb-5 ms-4">
+                {{ $module->name }}
             </div>
         </div>
         <div class="row g-0">
@@ -41,7 +44,7 @@
                                             <span class="caret">Лекции</span>
                                             <ul class="nested">
                                                 @foreach($user->homeworks as $homework)
-                                                    <li>{{ $homework->lection ? $homework->lection->title : null }}</li>
+                                                    <li>- {{ $homework->lection ? $homework->lection->title : null }}</li>
                                                 @endforeach
                                             </ul>
                                         </li>
